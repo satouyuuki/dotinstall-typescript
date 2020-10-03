4d91e913075e7f37db613e18a346ce15f0b146ca
satoyuki <yuki01377@gmail.com>
2020-10-03 15:28:27 +0900

    ジェネリクス

--- src/sample.ts
+++ src/sample.ts
@@ -1,25 +1,11 @@
-// Interface + Class
-interface GameUser {
-  score: number;
-  showScore(): void;
-}
+// Generics
 
-class User implements GameUser{
-  constructor(private _name: string, private _score: number) {
-  }
-  sayHi(): void {
-    console.log('hi! i am ' + this._name);
-  }
-  showScore(): void {
-    console.log('score is ' + this._score);
-  }
-  get name() {
-    return this._name;
-  }
-  get score() {
-    return this._score;
-  }
+// function getArray(value: number|string): (number|string)[] {
+//   return [value, value, value];
+// }
+
+function getArray<T>(value: T/*Type*/): T[] {
+  return [value, value, value];
 }
-const tom = new User('tom', 2);
-console.log(tom.name);
-tom.sayHi();
+console.log(getArray<number>(3));
+console.log(getArray<string>('hello'));
