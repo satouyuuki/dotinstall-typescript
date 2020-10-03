f84e674cef10d5cef14de1e758bce0a72882e5c9
satoyuki <yuki01377@gmail.com>
2020-10-03 10:31:38 +0900

    関数式

--- src/sample.ts
+++ src/sample.ts
@@ -12,3 +12,7 @@ function add(a: number, b?: number): number|undefined {
 // b ? b: number|undefinedになる
 console.log(add(5, 3));
 console.log(add(5));
+
+// 関数式(アロー関数)
+const diff = (a: number, b: number): number => a - b;
+console.log(diff(1, 2));
