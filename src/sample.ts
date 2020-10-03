06562e097c731f32096726fe6a89dd2ec16b91d7
satoyuki <yuki01377@gmail.com>
2020-10-03 15:37:43 +0900

    ジェネリクスに制約を

--- src/sample.ts
+++ src/sample.ts
@@ -1,11 +1,24 @@
 // Generics
-
-// function getArray(value: number|string): (number|string)[] {
-//   return [value, value, value];
-// }
-
-function getArray<T>(value: T/*Type*/): T[] {
-  return [value, value, value];
+interface Result {
+  a: number,
+  b: number
 }
-console.log(getArray<number>(3));
-console.log(getArray<string>('hello'));
+interface FinalResult {
+  a: number,
+  b: number,
+  c: string
+}
+class MyData<T extends Result> {// aとbさえ持っていればいい
+  constructor(private _value: T) { }
+  getArray(): T[] {
+    return [this._value, this._value, this._value];
+  }
+}
+// const v1 = new MyData<string>('hello');
+// console.log(v1.getArray());
+// const v2 = new MyData<number>(234);
+// console.log(v2.getArray());
+// const v3 = new MyData<FinalResult>({a: 22, b: 33});
+// console.log(v3.getArray());
+const v4 = new MyData<FinalResult>({a: 22, b: 33, c: 'hello world'});
+console.log(v4.getArray());
