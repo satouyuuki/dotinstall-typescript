3c98c5108426d879ffbdbc84852840c3a179ea80
satoyuki <yuki01377@gmail.com>
2020-10-03 16:03:45 +0900

    内部モジュール

--- src/sample.ts
+++ src/sample.ts
@@ -1,24 +1,15 @@
-// Generics
-interface Result {
-  a: number,
-  b: number
-}
-interface FinalResult {
-  a: number,
-  b: number,
-  c: string
-}
-class MyData<T extends Result> {// aとbさえ持っていればいい
-  constructor(private _value: T) { }
-  getArray(): T[] {
-    return [this._value, this._value, this._value];
-  }
-}
-// const v1 = new MyData<string>('hello');
-// console.log(v1.getArray());
-// const v2 = new MyData<number>(234);
-// console.log(v2.getArray());
-// const v3 = new MyData<FinalResult>({a: 22, b: 33});
-// console.log(v3.getArray());
-const v4 = new MyData<FinalResult>({a: 22, b: 33, c: 'hello world'});
-console.log(v4.getArray());
+// 内部モジュール
+// module UserModule {
+//   export const name = 'taguchi';
+//   export module AddressModule {
+//     export const zip = "111-1111";
+//   }
+// }
+
+/// <reference path="./user.ts" />
+
+console.log(UserModule.name);
+// console.log(UserModule.AddressModule.zip);
+
+import addr = UserModule.AddressModule;
+console.log(addr.zip);
---
+++ src/user.ts
@@ -0,0 +1,6 @@
+module UserModule {
+  export const name = 'taguchi';
+  export module AddressModule {
+    export const zip = "111-1111";
+  }
+}
