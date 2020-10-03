daa11487568cca769cdab044a67d0d460c3d0942
satoyuki <yuki01377@gmail.com>
2020-10-03 10:04:01 +0900

    init commit

---
+++ dist/sample.js
@@ -0,0 +1,11 @@
+"use strict";
+// string型
+var string = 'hello';
+// number型
+var number = 10;
+// any型
+var x;
+x = 10;
+x = 'hello';
+var numberArray;
+numberArray = [10, 5, 3];
---
+++ src/sample.ts
@@ -0,0 +1,13 @@
+// string型
+const string: string = 'hello';
+
+// number型
+const number: number = 10;
+
+// any型
+let x;
+x = 10;
+x = 'hello';
+
+let numberArray: number[];
+numberArray = [10, 5, 3];
